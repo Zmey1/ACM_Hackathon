0247346e1bdceac62463c5b2cd1f38e8e81f101e
Vimanyu Jain <vimanyujain07@gmail.com>
2025-03-14 01:32:48 +0530

    commit3

---
+++ lib/pages/resultpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Resultpage extends StatefulWidget {
+  const Resultpage({super.key});
+
+  @override
+  State<Resultpage> createState() => _ResultpageState();
+}
+
+class _ResultpageState extends State<Resultpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
